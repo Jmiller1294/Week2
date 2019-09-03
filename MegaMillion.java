fba8c208c3c879c7620a09653ba7ff926fe8ccae
Jmiller1294 <54234620+Jmiller1294@users.noreply.github.com>
2019-09-03 16:49:00 -0400

    Add files via upload

---
+++ MegaMillion.java
@@ -0,0 +1,28 @@
+package week2;
+
+/**
+ * 
+ * @author Justin Miller
+ *desc:
+ *date:
+ */
+//import the Scanner class
+import java.util.Random
+
+public class MegaMillion {
+	public static void main(String [] args)
+	
+	Random randGen = new random();
+
+	
+	//Declare variable to hold amount of dollar
+	//Declare variable for cost of ticket
+	//Prompt the user for the amount of dollars to bet on 
+	
+	//Store amount into amount of dollar variable
+	
+	//prompt user for six numbers and create variables for each of the six numbers
+	
+	//Randomly generate six numbers and compare
+
+}
